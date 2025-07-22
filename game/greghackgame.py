214ab83029dca24b31c763ef476efd526801e374
Wish <100239673+wish13yt@users.noreply.github.com>
2025-07-22 14:54:24 -0500

    greg

---
+++ game/greghackgame.py
@@ -0,0 +1,9 @@
+import os
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+print("greghack")
+print("by freakybob team")
+print("please visit greghack.freakybob.site for everything else, like clues to play, as this game is just a console")
+print("game.freakybob.site/greghack for information about the game")
+input("I have visited the website greghack.freakybob.site, as I must use it to play the game.")
+cls()
