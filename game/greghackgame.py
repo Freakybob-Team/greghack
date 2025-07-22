a66f75de5cca1b11bed40659a02ed5c8f67f75a4
Wish <100239673+wish13yt@users.noreply.github.com>
2025-07-22 15:12:35 -0500

    gaming

--- game/greghackgame.py
+++ game/greghackgame.py
@@ -1,6 +1,7 @@
 import os
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
+cmd = None
 print("greghack")
 print("by freakybob team")
 print("please visit greghack.freakybob.site for everything else, like clues to play, as this game is just a console")
@@ -12,7 +13,12 @@ print("Greg OS Command Prompt")
 print("Version 1.25") # increase first number every release, increase second number every year (ex: 1.25, 1 being release number, and 25 being year)
 print("Type help for help.")
 cmd = input()
+if (cmd == "hack"):
+    cls()
+    user = input("enter username: ")
+    website = input("enter website: ")
 if (cmd == "help"):
     cls()
     print("hack - hacking tool if you have all info")
     print("infosearch - search for info about people")
+    cmd = input()
